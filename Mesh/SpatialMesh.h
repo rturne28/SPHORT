160700a463adfc423fbe7113de70cd5087688a89
rturne28 <rturne28@tamu.edu>
2020-04-18 14:30:46 -0500

    tiny change

--- Mesh/SpatialMesh.h
+++ Mesh/SpatialMesh.h
@@ -5,13 +5,20 @@ class SpatialMesh
 {
   const double _radius;
   const int    _cells;
-  
+
 public:
   SpatialMesh(const double& radius, const int& cells)
   : _radius{ radius },
     _cells{ cells }
   {
   }
+
+  void fillCellList();
 };
+
+// Function that takes radius and # of cells data and creates a cell object
+// for each cell, calculates geometric info and applies flags, and places each
+// cell object in a container in this class. May also need to create an iterator
+// for this container, but it is possible that this can be done somewhere else
 
 #endif
